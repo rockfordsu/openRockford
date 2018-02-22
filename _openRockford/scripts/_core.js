4d65864f4bf5c00c55be77a6201c2ef4393ca196
Fudo Tsukiko <sampe.ma.ua@gmail.com>
2018-02-22 18:43:45 +0200

    corefirst

--- _openRockford/scripts/_core.js
+++ _openRockford/scripts/_core.js
@@ -5,3 +5,17 @@
 
 	===========================
 */
+
+const print = function (text) {
+	java.lang.System.out.print(
+		"[_openRockford] " + text
+	);
+
+	return true;
+}
+
+try {
+
+} catch(e) {
+	print("Error in "+__filename);
+}
